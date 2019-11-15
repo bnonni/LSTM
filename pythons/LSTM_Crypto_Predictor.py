ed5ed6e3592790a9db38a224721f38e163070025
bnonni <bryanwnonni@gmail.com>
2019-11-15 14:49:51 -0500

    commiting local changes

--- pythons/LSTM_Crypto_Predictor.py
+++ pythons/LSTM_Crypto_Predictor.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-# %matplotlib inline
+# In[394]:
+
+
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import RMSprop
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from url import URL
 from pymongo import *
 import statistics as stat
@@ -26,9 +27,7 @@ client = MongoClient(URL)
 db = client.crypto_wallet
 
 
-# In[2]:
-
-
+# In[395]:
 def checkLen(a, b):
     if len(a) == len(b):
         return True
@@ -36,14 +35,14 @@ def checkLen(a, b):
         return f'DB Objs:{len(a)} < Clean Arr Items:{len(b)}' if len(a) < len(b) else f'Clean Arr Items:{len(b)} < DB Objs:{len(a)}'
 
 
-# In[3]:
+# In[396]:
 
 
 def p(o):
     return print(o)
 
 
-# In[4]:
+# In[397]:
 
 
 def filterData(coll, st, narr):
@@ -56,7 +55,7 @@ def filterData(coll, st, narr):
     return narr
 
 
-# In[5]:
+# In[398]:
 
 
 def datetime_converter(dtstr):
@@ -65,46 +64,38 @@ def datetime_converter(dtstr):
     return int(epoch)
 
 
-# In[6]:
+# In[399]:
 
 
 BTC_Tickers_Collection = db.BTC_Tickers
 BTC_Tickers_Objs = list(BTC_Tickers_Collection.find())
-BTC_epochs = []
+BTC_dt_epochs = []
 BTC_prices = []
 BTC_volumes = []
 BTC_highs = []
 BTC_lows = []
 for obj in BTC_Tickers_Collection.find():
     dt = re.sub(r'\..*', '', obj.get('time')).replace('T', ' ').rstrip('Z')
-    BTC_epochs.append(datetime_converter(dt))
+    BTC_dt_epochs.append(datetime_converter(dt))
     BTC_prices.append(float(obj.get('price')))
     BTC_volumes.append(obj.get('volume'))
     BTC_highs.append(obj.get('ask'))
     BTC_lows.append(obj.get('bid'))
 
 
-# In[322]:
-
-
-for i,e in enumerate(BTC_epochs):
-    if i == 0:
-        pass
-    else:
-        BTC_epochs[i] = BTC_epochs[i-1] + 60
-
-
-# In[323]:
+# In[400]:
 
 
 p(checkLen(BTC_Tickers_Objs, BTC_prices))
-p(checkLen(BTC_epochs, BTC_prices))
+p(checkLen(BTC_dt_epochs, BTC_prices))
 p(checkLen(BTC_Tickers_Objs, BTC_volumes))
 p(checkLen(BTC_Tickers_Objs, BTC_highs))
 p(checkLen(BTC_Tickers_Objs, BTC_lows))
 
 
-# In[324]:
+# In[401]:
+
+
 BTC_RSI_Collection = db.BTC_RSI14_Data
 BTC_RSI_Objs = list(BTC_RSI_Collection.find())
 BTC_RSIs = []
@@ -127,20 +118,20 @@ for rsio in BTC_RSI_Collection.find():
         sys.exit(1)
 
 
-# In[325]:
+# In[402]:
 
 
 if len(Errors) > 0:
     print(Errors)
 
 
-# In[326]:
+# In[403]:
 
 
 p(checkLen(BTC_RSI_Objs, BTC_RSIs))
 
 
-# In[327]:
+# In[404]:
 
 
 BTC_ADL_Collection = db.BTC_ADL_Data
@@ -168,17 +159,12 @@ for o in BTC_ADL_Collection.find():
         sys.exit(1)
 
 
-# In[328]:
-
-
+# In[405]:
 p(checkLen(BTC_ADL_Objs, BTC_ADLs))
-
 p(checkLen(BTC_ADL_slope, BTC_ADLs))
 
 
-# In[329]:
-
-
+# In[406]:
 BTC_OBV_Collection = db.BTC_OBV_Data
 BTC_OBV_Objs = list(BTC_OBV_Collection.find())
 BTC_OBVs = []
@@ -204,22 +190,21 @@ for o in BTC_OBV_Collection.find():
         sys.exit(1)
 
 
-# In[330]:
-
-
+# In[407]:
 p(checkLen(BTC_OBV_Objs, BTC_OBVs))
 p(checkLen(BTC_OBV_slope, BTC_OBVs))
 
 
-# In[331]:
+# In[408]:
 
+print(f'datetime: {len(BTC_dt_epochs)}\nprices: {len(BTC_prices)}')
 
-print(f'datetime: {len(BTC_epochs)}\nprices: {len(BTC_prices)}')
 collection_lengths = [len(BTC_volumes), len(BTC_highs), len(BTC_lows), len(BTC_ADLs), len(BTC_ADL_slope), len(BTC_OBVs), len(BTC_OBV_slope)]
+
 print(f'Volumes: {len(BTC_RSIs)}\nHighs: {len(BTC_RSIs)}\nLows: {len(BTC_RSIs)}\nRSI: {len(BTC_RSIs)}\nADL_slp: {len(BTC_ADL_slope)}\nOBV_slp: {len(BTC_OBV_slope)}')
 
 
-# In[332]:
+# In[409]:
 
 
 min = collection_lengths[0]
@@ -228,89 +213,107 @@ for i in range(1, len(collection_lengths)):
         min = collection_lengths[i]
 
 
-# In[342]:
+# In[410]:
 # BTC_Data = { 'Datetime': BTC_dt_epochs[0:min], 'Prices': BTC_prices[0:min], 'High': BTC_highs[0:min], 'Low': BTC_lows[0:min] }
 
-BTC_Data = { 'Datetime': BTC_epochs[0:min],
-             'Prices': BTC_prices[0:min],
-             'Volumes':BTC_volumes[0:min],
-             'High':BTC_highs[0:min],
-             'Low':BTC_lows[0:min],
-             'RSI': BTC_RSIs[0:min],
-             'ADL' : BTC_ADLs[0:min],
-             'ADL_slope': BTC_ADL_slope[0:min],
-             'OBV' : BTC_OBVs[0:min],
-             'OBV_slope': BTC_OBV_slope[0:min] }
+BTC_Data = {'Datetime': BTC_dt_epochs[0:min],
+            'Prices': BTC_prices[0:min],
+            'Volumes':BTC_volumes[0:min],
+            'High':BTC_highs[0:min],
+            'Low':BTC_lows[0:min],
+            'RSI': BTC_RSIs[0:min],
+            'ADL' : BTC_ADLs[0:min],
+            'ADL_slope': BTC_ADL_slope[0:min],
+            'OBV' : BTC_OBVs[0:min],
+            'OBV_slope': BTC_OBV_slope[0:min]}
 
+# In[411]:
 keys = BTC_Data.keys()
 for k in keys:
     print(f'{k}: {len(BTC_Data[k])}')
 
 
-# In[343]:
-
+# In[412]:
 BTC_df = pd.DataFrame(BTC_Data)
 BTC_df.head()
 
-# In[344]:# x = BTC_df.Datetime# y = BTC_df.Prices# plt.plot(x, y)# plt.show()
-# In[346]:
+# In[413]:
+BTC_df.describe()
+
+# In[414]:
+BTC_df.info()
+
+# In[415]:
+plt.plot( BTC_df.Datetime[0:1000], BTC_df.Prices[0:1000])
+
+# In[417]:
+BTC_df['Volumes'] = BTC_df.Volumes.astype('float64')
 BTC_df['High'] = BTC_df.High.astype('float64')
 BTC_df['Low'] = BTC_df.Low.astype('float64')
-BTC_df['Volumes'] = BTC_df.Volumes.astype('float64')
 
-ADL_avg = BTC_df.ADL.mean()
+# In[419]:
+# ADL_avg = BTC_df.ADL.mean()
+# ADL_slp_avg = BTC_df.ADL_slope.mean()
 RSI_avg = BTC_df.RSI.mean()
-ADL_slp_avg = BTC_df.ADL_slope.mean()
 OBV_slp_avg = BTC_df.OBV_slope.mean()
 
-values = {'OBV_slope': OBV_slp_avg, 'RSI': RSI_avg}
+# In[420]:
+values = {'OBV_slope': OBV_slp_avg, 'RSI': RSI_avg }
 BTC_df = BTC_df.fillna(value=values)
 BTC_df.head()
 
-# In[348]:
+# In[421]:
 BTC_df.info()
 
-# In[384]:
+# In[423]:
 np.random.seed(42)
-# X = BTC_df('Prices', axis=1).values
-X = BTC_df[['High', 'Low']].values
+
+# In[426]:
+X = BTC_df.drop('Prices', axis=1).values
 y = BTC_df['Prices'].values
 print(X.shape)
 print(y.shape)
 
-# In[384]:
-# split into train & test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+# In[416]:
+plt.plot( X.Datetime, y)
 
-# set scalar & scale train, test data
+# In[430]:
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=False)
+
+# In[416]:
+plt.plot( X_train, y_train)
+
+# In[431]:
 scalar = MinMaxScaler(feature_range=(0, 1))
 X_train = scalar.fit_transform(X_train)
 X_test = scalar.fit_transform(X_test)
 
-# reshape train, test data to 3D
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-# In[389]:
+# In[432]:
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1],  1))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1],  1))
+
+
+# In[385]:
 model = Sequential()
 
-# input layer
+#input layer
 model.add(LSTM(200, return_sequences=True, input_shape=(X_train.shape[1], 1)))
 model.add(Dropout(0.2))
 
-# hidden layer
+#hidden layer
 model.add(LSTM(200, return_sequences=True))
 model.add(Dropout(0.2))
 
-# # hidden layer
+# #hidden layer
 model.add(LSTM(200, return_sequences=True))
 model.add(Dropout(0.2))
 
-# # hidden layer
+# #hidden layer
 model.add(LSTM(200, return_sequences=True))
 model.add(Dropout(0.2))
 
-# # hidden layer
+# #hidden layer
 model.add(LSTM(200, return_sequences=True))
 model.add(Dropout(0.2))
 
@@ -321,16 +324,17 @@ model.add(Dropout(0.2))
 # output layer
 model.add(Dense(1))
 
+
 # In[346]:
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mape'])
 print(model.summary())
 
 
-# In[350]:
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=300, shuffle=True, batch_size=32)
+# In[ ]:
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, shuffle=False, batch_size=25)
 
 
-# In[354]:
+# In[ ]:
+model_loss = model.evaluate(X_test, y_test, verbose=0)
+print(model_loss)
 
-eval = model.evaluate(X_test, y_test, verbose=2)
-print(f'loss: {eval}')
